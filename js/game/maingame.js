754bac3495a439a2f10c309eebd6797174db0407
Frank Paschen <frankpaschen99@gmail.com>
2017-02-01 23:19:28 -0500

    few more hours and ill have this shit done

--- js/game/maingame.js
+++ js/game/maingame.js
@@ -9,10 +9,13 @@ MainGame.prototype = {
 
 
 		game.physics.startSystem(Phaser.Physics.Arcade);
+		game.physics.startSystem(Phaser.Physics.P2JS);
+		game.physics.p2.setImpactEvents(true);
 
-		game.physics.arcade.gravity = new Phaser.Point(0, 1.622);
+		game.physics.arcade.gravity = new Phaser.Point(0, 3);
 
 		buildWorld();
+		game.camera.follow(player.sprite);
 
     },
 
@@ -23,10 +26,23 @@ MainGame.prototype = {
 
 function buildWorld() {
 	/* contigous level */
-	for (var count = 0, i = 0; i < 25; i++, count+= 1267)
-		game.add.sprite(count, 1080-390, 'level');
-	for (var count = -1267, i = 0; i < 25; i++, count-= 1267)
-		game.add.sprite(count, 1080-390, 'level');
+	for (var count = 0, i = 0; i < 25; i++, count+= 1267) {
+		var sprite = game.add.sprite((1920-1267)+count, 1080-191, 'level');
+		game.physics.p2.enable(sprite, false);
+		sprite.body.clearShapes();
+
+		sprite.body.loadPolygon('level_physics', 'level');
+		sprite.body.static = true;
+	}
+
+	for (var count = -1267, i = 0; i < 25; i++, count-= 1267) {
+		var sprite = game.add.sprite((1920-1267)+count, 1080-191, 'level');
+		game.physics.p2.enable(sprite, false);
+		sprite.body.clearShapes();
+
+		sprite.body.loadPolygon('level_physics', 'level');
+		sprite.body.static = true;
+	}
 }
 
 class Player {
@@ -34,11 +50,12 @@ class Player {
 		this.sprite = game.add.sprite(posX, posY, 'lander');
 		game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
 		this.sprite.anchor.setTo(0.5, 0.5);
+		this.cameraMoved = false;
 	}
 	update(deltaTime) {
 		// collide with ground here and handle input
 		if (game.input.keyboard.isDown(Phaser.Keyboard.W))
-	        game.physics.arcade.accelerationFromRotation(this.sprite.rotation-90, 10, this.sprite.body.acceleration);
+	        game.physics.arcade.accelerationFromRotation(this.sprite.rotation-90, 20, this.sprite.body.acceleration);
 
 	    if (game.input.keyboard.isDown(Phaser.Keyboard.A))
 	        this.sprite.body.angularVelocity = -300;
@@ -46,6 +63,12 @@ class Player {
 	        this.sprite.body.angularVelocity = 300;
 	    else
 	        this.sprite.body.angularVelocity = 0;
+
+		if (this.sprite.x < 0 && !this.cameraMoved) {
+			game.camera.x -= 1920;
+			this.cameraMoved = true;
+		}
+
 	}
 	success() {
 		// reset their position
@@ -54,3 +77,14 @@ class Player {
 		// blow them up
 	}
 }
+
+
+/****
+
+TODO: fix upwards velocity against gravity being waaay too strong or something else is
+wrong idfk
+
+TODO: allow collision with the ground. Figure out p2js and arcade
+
+TODO: Camera movement
+****/
