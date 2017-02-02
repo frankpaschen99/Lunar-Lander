d2f6e5c1a577d1a1e3f55993df6e7296cb6cc51b
Frank Paschen <frankpaschen99@gmail.com>
2017-02-01 22:40:25 -0500

    player movement

--- js/game/maingame.js
+++ js/game/maingame.js
@@ -5,7 +5,7 @@ var player;
 MainGame.prototype = {
     create: function () {
 
-		player = new Player(0, 0);
+		player = new Player(10, 10);
 
 
 		game.physics.startSystem(Phaser.Physics.Arcade);
@@ -33,10 +33,19 @@ class Player {
 	constructor(posX, posY) {
 		this.sprite = game.add.sprite(posX, posY, 'lander');
 		game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+		this.sprite.anchor.setTo(0.5, 0.5);
 	}
 	update(deltaTime) {
-		// collide with ground here
+		// collide with ground here and handle input
+		if (game.input.keyboard.isDown(Phaser.Keyboard.W))
+	        game.physics.arcade.accelerationFromRotation(this.sprite.rotation-90, 10, this.sprite.body.acceleration);
 
+	    if (game.input.keyboard.isDown(Phaser.Keyboard.A))
+	        this.sprite.body.angularVelocity = -300;
+	    else if (game.input.keyboard.isDown(Phaser.Keyboard.D))
+	        this.sprite.body.angularVelocity = 300;
+	    else
+	        this.sprite.body.angularVelocity = 0;
 	}
 	success() {
 		// reset their position
