f1f6647ff2cdce84df9683b852b21658b83aa9b9
Frank Paschen <frankpaschen99@gmail.com>
2017-02-02 23:05:00 -0500

    goodnight

--- js/game/maingame.js
+++ js/game/maingame.js
@@ -8,27 +8,28 @@ MainGame.prototype = {
 
 		cursors = game.input.keyboard.createCursorKeys();
 
+		/* Setup physics */
 		game.physics.startSystem(Phaser.Physics.P2JS);
-
 		game.physics.p2.setImpactEvents(true);
-
 		game.physics.p2.gravity.y = 20;
 		
-		
+		// Create player lander
 		player = new Player(1000, 10);
-		
+
+		// Generate world w/ polygon collider
 		buildWorld();
+		
+		// Used for camera movement
 		game.world.setBounds(0, 0, 30408, 2000);
 
     },
-
     update: function () {
 		player.update();
     }
 };
 
 function buildWorld() {
-	/* contigous level */
+	/* Generate contiguous level */
 	for (var count = 0, i = 0; i < 25; i++, count+= 1267) {
 		var sprite = game.add.sprite((1920-1267)+count, 1080-200, 'level');
 		game.physics.p2.enable(sprite, true);
@@ -36,7 +37,6 @@ function buildWorld() {
 
 		sprite.body.loadPolygon('level_physics', 'level');
 		sprite.body.static = true;
-		
 	}
 }
 
@@ -46,8 +46,8 @@ class Player {
 		this.sprite = game.add.sprite(posX, posY, 'lander');
 		
 		game.physics.p2.enable(this.sprite, true);
-
-		this.sprite.body.onBeginContact.add(landerCollision);
+		// Collision callback
+		this.sprite.body.onBeginContact.add(this.landerCollision);
 
 	}
 	update(deltaTime) {
@@ -59,21 +59,10 @@ class Player {
 	fail() {
 		// blow them up
 	}
+	landerCollision(body, shape1, shape2, equation) {
+		// handle colliding
+		// call success() or fail()
+	}	
 }
 
-function landerCollision(body, shape1, shape2, equation) {
-	console.log("called");
-	console.log(body);
-}
 
-/****
-
-TODO: fix upwards velocity against gravity being waaay too strong or something else is
-wrong idfk
-
-TODO: allow collision with the ground. Figure out p2js and arcade
-
-TODO: Camera movement
-
-TODO: fix everything
-****/
